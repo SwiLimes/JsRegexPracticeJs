f401e09a2c7db22c82d89557a32ebba3c6e0edbb
SwiLimes <nikitos15052001@gmail.com>
2022-08-29 12:09:13 +0300

    Вывод таблицы районов и городов

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
+let containerBlock = document.querySelector('.container');
+
+let activeElement = null;
+
+let list = null;
+
+let table;
+
+
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        let list = await load();
-        listToTable(filter(list, true), '.container');
-    } catch (error) {
-        throw new Error(error);
-    }
+    // try {
+    list = await load();
+
+    displayTable(list, 'regionTable', '.container');
+    // displaySubTable(list);
+    // } catch (error) {
+    //     throw new Error(error);
+    // }
 
 })
 
@@ -14,13 +25,12 @@ async function load() {
     let text = await response.text();
 
     // return createListBySplit(text);
-    console.log(createListByRegex(text));
     return createListByRegex(text);
 };
 
 let printList = function (list) {
     let pre = document.createElement('pre');
-    for(let city of list) {
+    for (let city of list) {
         let p = document.createElement('p');
         p.textContent = city.code + ' ' + city.levelCity + ' ' + city.nameCity;
         pre.appendChild(p);
@@ -49,7 +59,7 @@ function createListByRegex(text) {
     let list = [];
     const regex = /(?<code1>\d+)";"(?<code2>\d+)";"(?<code3>\d+)";"(?<code4>\d+)";"\d+";"(?<lvl>\d+)";"(?<name>.*?)"/g;
     let matches = text.matchAll(regex);
-    for(let match of matches) {
+    for (let match of matches) {
         let {code1, code2, code3, code4, lvl, name} = match.groups;
         list.push({
             code1: code1,
@@ -64,48 +74,67 @@ function createListByRegex(text) {
     return list;
 }
 
-function filter(list, showRegion = false, showCity = false) {
-    if(showRegion) {
-        let filteredList = list.filter(item => {
-            return item.code2 == '000' && item.code3 == '000' && item.code4 == '000' && item.levelCity == '1';
-        });
-        return filteredList;
-    }
+function filter(list, currentElement, showRegion, showCity) {
+    if (currentElement)
+        if (showRegion && currentElement) {
+            let elementCode1 = currentElement.childNodes[0].innerHTML.split(' ')[0];
+            console.log(elementCode1);
+            let filteredList = list.filter(item => {
+                return item.code1 == elementCode1
+                    && item.code2 != '000'
+                    && item.code4 == '000'
+                    && item.levelCity == '1';
+            });
+            return filteredList;
+        }
 
-    if(showCity) {
+    if (showCity && currentElement) {
+        let elementCode2 = currentElement.childNodes[0].innerHTML.split(' ')[1];
         let filteredList = list.filter(item => {
-            return item.code4 != '000' && item.levelCity == '2';
+            return item.code2 == elementCode2
+                && item.code4 != '000'
+                && item.levelCity == '2';
         })
         return filteredList;
     }
+    return list.filter(item => {
+        return item.code2 == '000' && item.levelCity == '1';
+    })
 }
-function listToTable(list, className) {
-    let table = document.createElement('table');
-    table.setAttribute('class', 'table')
-    let tHead = document.createElement('thead');
+
+
+function displayTable(currentList, tableName, className) {
+    table = document.createElement('table');
+    table.classList.add(tableName);
+
     let tBody = document.createElement('tbody');
-    table.appendChild(tHead);
     table.appendChild(tBody);
 
 
-    let headerCode = document.createElement('th');
-    headerCode.innerHTML = 'Code';
-    let headerLvl = document.createElement('th');
-    headerLvl.innerHTML = 'Level';
-    let headerName = document.createElement('th');
-    headerName.innerHTML = 'Name';
+    if (tableName == 'regionTable') {
+        let tHead = document.createElement('thead');
 
-    let tableRowHeaders = document.createElement('tr');
-    tableRowHeaders.appendChild(headerCode);
-    tableRowHeaders.appendChild(headerLvl);
-    tableRowHeaders.appendChild(headerName);
+        table.appendChild(tHead);
 
-    tHead.appendChild(tableRowHeaders);
+        let headerCode = document.createElement('th');
+        headerCode.innerHTML = 'Code';
+        let headerLvl = document.createElement('th');
+        headerLvl.innerHTML = 'Level';
+        let headerName = document.createElement('th');
+        headerName.innerHTML = 'Name';
+
+        let tableRowHeaders = document.createElement('tr');
+        tableRowHeaders.appendChild(headerCode);
+        tableRowHeaders.appendChild(headerLvl);
+        tableRowHeaders.appendChild(headerName);
+
+        tHead.appendChild(tableRowHeaders);
+
+        currentList = filter(list, null, true, false);
+    }
 
 
-    for(let item of list) {
-        // console.log(item.code);
-
+    for (let item of currentList) {
         let tableDataCode = document.createElement('td');
         tableDataCode.innerHTML = item.code;
         let tableDataLvl = document.createElement('td');
@@ -124,23 +153,56 @@ function listToTable(list, className) {
     let tableBlock = document.querySelector(className);
     tableBlock.appendChild(table);
 
+    if (tableName == 'regionTable') {
+        tBody.addEventListener('click', event => {
+            let selectedRow = event.target.closest('tr');
+            if (!selectedRow) return;
+            if (activeElement) {
+                activeElement.classList.remove('active');
+            }
+
+            activeElement = selectedRow;
+            activeElement.classList.add('active');
+            displaySubTable(list, activeElement);
+        });
+    }
 
 }
 
-let inputRegion = document.querySelector('#region');
-inputRegion.oninput = () => {
-    let rows = document.getElementsByTagName('tr');
-    if(inputRegion.value.length < 3) {
-        for(let row of rows) {
-            row.classList.remove('hidden');
-        }
-    };
+//
+function displaySubTable(currentList, selectedElement) {
+    let subTableList = document.querySelector('.subTable');
+    if (subTableList) subTableList.remove();
 
-    for(let row of rows) {
-        let rowContent = row.cells.item(2).textContent;
-        if (!rowContent.includes(inputRegion.value)) row.classList.add('hidden');
-    }
-};
+    subTableList = filter(list, selectedElement, true, false);
+    if (subTableList.length == 0) return;
+    displayTable(subTableList, 'subTable', '.container');
+
+    let subTable = document.querySelector('.subTable');
+    subTable.addEventListener('click', event => {
+        let selectedDistrict = event.target.closest('tr');
+
+        let cityTable = document.querySelector('.cityTable');
+        if (cityTable) cityTable.remove();
+        displayTable(filter(list, selectedDistrict, false, true), 'cityTable', '.container');
+    })
+}
+
+// let inputRegion = document.querySelector('#region');
+// inputRegion.oninput = () => {
+//     let rows = document.getElementsByTagName('tr');
+//     if(inputRegion.value.length < 3) {
+//         for(let row of rows) {
+//             row.classList.remove('hidden');
+//         }
+//     };
+//
+//     for(let row of rows) {
+//         let rowContent = row.cells.item(2).textContent;
+//         if (!rowContent.includes(inputRegion.value)) row.classList.add('hidden');
+//     }
+// };
+
 
 
 
